33bb668b42dd662f6e8a791466e58d4d8a2b7993
djachenkoroman <emessage@rambler.ru>
2023-04-30 22:16:32 +0300

    Изменил pointnet_train_ch3.py

--- pointnet_train_ch3.py
+++ pointnet_train_ch3.py
@@ -27,13 +27,26 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device', type=str, default='cuda', help='current device [default=cuda]')
-parser.add_argument('--moddir', type=str, default='', help='models directory [default=cuda]')
+parser.add_argument('--moddir', type=str, default='', help='models directory [default=null]')
+parser.add_argument('--epochs', type=int, default=10, help='epochs [default=10]')
 
 args = parser.parse_args()
 
+params = {
+    'batch_size':4,
+    'num_workers':1,
+    'shuffle':True,
+    'output_file':'terra_model',
+    'epochs':args.epochs,
+    'lr':0.001
+}
+
+
 if __name__ == '__main__':
-    print(args.device)
-    print(args.moddir)
+    print("device: {0}".format(args.device))
+    print("models directory: {0}".format(args.moddir))
 
     if not os.path.isdir(args.moddir):
         sys.exit("moddir not found")
+
+    print("epochs: {0}".format(args.moddir))
