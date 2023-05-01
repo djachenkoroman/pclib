5ba3b58f110173ceb68f9d570d264761abfe0cec
djachenkoroman <emessage@rambler.ru>
2023-05-01 13:58:58 +0300

    Изменил fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -10,4 +10,17 @@ parser.add_argument('--dsfile', type=str, default='', help='dataset file [defaul
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    pass
+    # получим объект файла
+    file1 = open(args.dsfile, "r")
+
+    for i in range(10):
+        # считываем строку
+        line = file1.readline()
+        # прерываем цикл, если строка пустая
+        if not line:
+            break
+        # выводим строку
+        print(line.strip())
+
+    # закрываем файл
+    file1.close
