15d47ce825bb352b55d196b26def5793b4d5285b
djachenkoroman <emessage@rambler.ru>
2023-05-07 20:14:27 +0300

    Изменил pointnet_train.py.py

--- pointnet_train.py
+++ pointnet_train.py
@@ -212,6 +212,6 @@ if __name__ == '__main__':
 
     if args.creategraph==True:
         import matplotlib.pyplot as plt
-        plt.plot(m_loss)
+        plt.plot(m_loss.T)
         plt.show()
         plt.savefig(os.path.join(moddir,"loss_{0}_ch{1}_gs{2}_nc{3}_pn{4}.png".format(date_id, args.channels, args.gridsize, num_classes, args.npoints)))
