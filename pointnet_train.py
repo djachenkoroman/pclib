c14e4c09e9aa79a10d1231a406ae6e36bf13958f
djachenkoroman <emessage@rambler.ru>
2023-05-08 22:48:10 +0300

    Изменил pointnet_train.py

--- pointnet_train.py
+++ pointnet_train.py
@@ -54,7 +54,7 @@ def pointnet_train(
         epochs=10,
         creategraph=True
     ):
-
+    start=datetime.datetime.now()
     params = {
         'batch_size': 4,
         'num_workers': 1,
@@ -205,7 +205,9 @@ def pointnet_train(
         plt.plot(data[:,[1]])
         plt.savefig(os.path.join(currdir,"gr_{0}_ch{1}_gs{2}_nc{3}_np{4}_ep{5}.png".format(date_id, channels, gridsize, num_classes, npoints, epochs)))
 
-    return loss.item(),accuracy
+    end=datetime.datetime.now()
+    sec= (end - start).total_seconds()
+    return round(loss.item(),4),round(accuracy,4),sec
 
 
 if __name__ == '__main__':
