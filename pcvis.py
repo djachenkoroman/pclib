bc4d720c4276e6e0175164edff8bb8b0a333e4a7
djachenkoroman <127408946+djachenkoroman@users.noreply.github.com>
2024-01-12 12:31:51 +0300

    Update pcvis.py
    line окантовка

--- pcvis.py
+++ pcvis.py
@@ -119,7 +119,8 @@ def pcshow_xyzl(data, x_column=0, y_column=1, z_column=2, label_column=6, frac=1
     data = [go.Scatter3d(x=xs, y=ys, z=zs, mode='markers')]
     fig = visualize_rotate(data)
     fig.update_traces(marker=dict(
-                        size=2,
+                        size=10,
+                        line=dict(size=3, color='blue'),
                         color=[cdict[label] for label in labels]),
                       selector=dict(mode='markers'))
     fig.show()
